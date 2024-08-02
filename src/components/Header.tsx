abcb743b8ff585dc1bfc5ad332c8a68ab42c26d6
cogito1016 <cogito1016@naver.com>
2024-08-03 00:14:41 +0900

    [feature] 헤더UI (kan-17)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,33 @@
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import LinkItem from "./LinkItem";
-import Logo from "./Logo";
-import Navigation from "./Navigation";
-
-const navLinks = [
-  { to: "/", text: "AI 도우미" },
-  { to: "/", text: "채용 정보" },
-  { to: "/", text: "채용 지도" },
-  { to: "/", text: "나의 정보" },
-  { to: "/", text: "교육 정보" },
-];
-
-const loginLink = [{ to: "/login", text: "로그인" }];
+import styles from "../css/Header.module.css";
+import logo from "../asset/logo.svg";
 
 const Header = ({}) => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={3} padding={2}>
-        <Grid item xs>
-          <Logo text="InAble" />
-        </Grid>
-        <Grid item xs={5}>
-          <Navigation links={navLinks}></Navigation>
-        </Grid>
-        <Grid item xs>
-          <LinkItem to={loginLink[0].to} text={loginLink[0].text}></LinkItem>
-        </Grid>
-      </Grid>
-    </Box>
+    <div className={styles.gnb}>
+      <div className={styles.logoParent}>
+        <img className={styles.logoIcon} alt="" src={logo} />
+        <div className={styles.tabParent}>
+          <div className={styles.tab}>
+            <div className={styles.text}>AI 도우미</div>
+          </div>
+          <div className={styles.tab}>
+            <div className={styles.text}>채용 정보</div>
+          </div>
+          <div className={styles.tab}>
+            <div className={styles.text}>채용 지도</div>
+          </div>
+          <div className={styles.tab}>
+            <div className={styles.text}>나의 정보</div>
+          </div>
+          <div className={styles.tab}>
+            <div className={styles.text}>교육 정보</div>
+          </div>
+        </div>
+        <div className={styles.tab}>
+          <div className={styles.text}>로그인</div>
+        </div>
+      </div>
+    </div>
   );
 };
 
