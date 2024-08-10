c3bd868c5d403dd12aad421ddf122b93cfc7703e
Hyeonjiwon <zlone1208@gmail.com>
2024-08-10 17:29:18 +0900

    [feat] Naver 지도 API 호출 하고 채용정보 마커 표시 (kan-14) Motivation: - 채용지도 화면 개발 - 'index.tsx' 네이버 지도 API 키 .env에서 읽어오도록 수정 - 'typescript-plugin-css-modules', 'styled-component' 추가
    Modification:
    - 'JobPostMap.tsx' 추가
    - 'JobPostMapPage.tsx' 추가
    - 'JobPostDataType.ts' 추가
    - 'index.tsx' 수정

---
+++ src/pages/JobPostMapPage.tsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import JobPostMap from "../components/JobPostMap";
+import { JobPostDataType } from "../types/JobPostDataType";
+
+const JobPostMapPage = () => {
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [jobPostData, setJobPostData] = useState<JobPostDataType[]>([]);
+
+  useEffect(() => {
+    // API 호출 함수
+    const fetchJobPostData = async () => {
+      try {
+        const response = await axios.get("http://127.0.0.1:8000/job_posts/");
+        const data = response.data;
+
+        // 받아온 데이터를 원하는 형식으로 변환
+        const transformedData = data["job_posts"].map((item: any) => ({
+          _id: item.id,
+          title: item.busplaName,
+          address: { area: item.compAddr, fullAddress: item.compAddr },
+          lat: parseFloat(item.latitude),
+          lng: parseFloat(item.longitude),
+        }));
+
+        setJobPostData(transformedData);
+      } catch (error) {
+        console.error("Failed to fetch job post data:", error);
+      }
+    };
+
+    fetchJobPostData();
+    setSearchKeyword("서울시 마포구");
+  }, []);
+
+  // 초기 좌표 설정 (전체 지도 보기용)
+  const initialCoordinates = { latitude: 37.5665, longitude: 126.978 };
+
+  return (
+    <>
+      <h1>지도</h1>
+      <br></br>
+      <JobPostMap coordinates={initialCoordinates} jobPostData={jobPostData} />
+    </>
+  );
+};
+
+export default JobPostMapPage;
