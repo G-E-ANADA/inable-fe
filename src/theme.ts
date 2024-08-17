80c75afa602b2503416acbfff8d08942d49a7ce9
Hyeonjiwon <zlone1208@gmail.com>
2024-08-17 18:46:16 +0900

    [style] 프로젝트 전체 테마 설정 추가 (kan-28)
    Motivation:
    - MUI 테마 및 프로젝트 전체 스타일 설정
    
    Modification:
    - 'theme.ts' 파일에 MUI 테마 설정 추가 기본 색상, 텍스트 색상, 폰트 패밀리 등을 설정
    - 'App.tsx'에 적용

---
+++ src/theme.ts
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#224A99",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+    text: {
+      primary: "#000000", // 기본 글자 색상 설정
+    },
+  },
+  typography: {
+    fontFamily: '"Noto Sans CJK KR", sans-serif',
+  },
+});
+
+export default theme;
