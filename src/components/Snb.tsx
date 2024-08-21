c1cd7a7461add897213ef7b57688a796dc405bfd
cogito1016 <cogito1016@naver.com>
2024-08-22 03:15:33 +0900

    [feat] 나의정보>SNB영역 선택기능 & 선택후 UI변경 (kan-41)

--- src/components/Snb.tsx
+++ src/components/Snb.tsx
@@ -1,16 +1,28 @@
 import { FunctionComponent } from "react";
 import styles from "../css/SNB.module.css";
 
-const SNB: FunctionComponent = () => {
+const SNB = ({ handleMenuSelect, selectedMenu }: any) => {
   return (
     <div className={styles.snb}>
-      <div className={styles.text}>
-        <b className={styles.text1}>자기 관리</b>
+      <div
+        className={`${styles.snbBox} ${styles.selectBox} 
+        ${selectedMenu === "SELF_MANAGEMENT" ? styles.activeSnb : ""}`}
+        onClick={() => handleMenuSelect("SELF_MANAGEMENT")}
+      >
+        <div className={styles.text1}>자기 관리</div>
       </div>
-      <div className={styles.text2}>
+      <div
+        className={`${styles.snbBox} ${styles.selectBox}
+        ${selectedMenu === "APPLY_ACTIVITY" ? styles.activeSnb : ""}`}
+        onClick={() => handleMenuSelect("APPLY_ACTIVITY")}
+      >
         <div className={styles.text1}>지원 활동</div>
       </div>
-      <div className={styles.text2}>
+      <div
+        className={`${styles.snbBox} ${styles.selectBox}
+        ${selectedMenu === "COMMUNITY" ? styles.activeSnb : ""}`}
+        onClick={() => handleMenuSelect("COMMUNITY")}
+      >
         <div className={styles.text1}>커뮤니티</div>
       </div>
     </div>
