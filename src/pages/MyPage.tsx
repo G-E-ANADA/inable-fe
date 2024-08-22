2278b502492785a37fb562504e86754b4c5beb06
unknown <jieun98n@gmail.com>
2024-08-23 01:39:40 +0900

    [feat]나의 정보 화면 업데이트 (kan-41)

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -3,6 +3,9 @@ import styles from "../css/MyPage.module.css";
 import Header from "../components/Header";
 import SNB from "../components/Snb";
 import { Checkbox } from "@mui/material";
+import MyInfo from "../components/MyInfo";
+import ApplyActivity from "../components/ApplyActivity";
+import Community from "../components/Community";
 
 type MyPageSNBMenu = "SELF_MANAGEMENT" | "APPLY_ACTIVITY" | "COMMUNITY";
 
@@ -16,56 +19,16 @@ const MyPage: FunctionComponent = () => {
   return (
     <>
       <Header />
-      <SNB handleMenuSelect={handleMenuSelect} selectedMenu={selectedMenu} />
 
-      {selectedMenu === "SELF_MANAGEMENT" && (
-        <div className={styles.card}>
-          <div className={styles.textInputContainer}>
-            <div className={styles.div}>나의 정보</div>
-            <div className={styles.textInputContainer1}>
-              <div className={styles.textInput} />
-              <div className={styles.btn}>
-                {/* <div className={styles.text}>이력서 작성하기</div> */}
-                <a className={styles.btn} href="/resumeWriteForm">
-                  이력서 작성하기
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className={styles.container}>
-            <div className={styles.div}>나의 관심사</div>
-            <div className={styles.horizontalContainer}>
-              <div className={styles.component2Parent}>
-                <div className={styles.component2}>
-                  <div className={styles.text}>저장된 공고</div>
-                </div>
-                <div className={styles.component3}>
-                  <div className={styles.text2}>관심 기업</div>
-                </div>
-                <div className={styles.component3}>
-                  <div className={styles.text2}>관심 교육</div>
-                </div>
-              </div>
-              <div className={styles.textInput1}>
-                <div className={styles.button}>
-                  <Checkbox color="primary" size="medium" />
-                  <div className={styles.text2}>전체 선택</div>
-                </div>
-                <div className={styles.button2}>
-                  <div className={styles.horizontalContainer1}>
-                    <div className={styles.button3}>삭제</div>
-                  </div>
-                </div>
-              </div>
-              <div className={styles.textInput2} />
-            </div>
-          </div>
-        </div>
-      )}
+      <div className={styles.contentContainer}>
+        <SNB handleMenuSelect={handleMenuSelect} selectedMenu={selectedMenu} />
 
-      {selectedMenu === "APPLY_ACTIVITY" && <div>APPLY_ACTIVITY</div>}
+        {selectedMenu === "SELF_MANAGEMENT" && <MyInfo></MyInfo>}
 
-      {selectedMenu === "COMMUNITY" && <div>COMMUNITY</div>}
+        {selectedMenu === "APPLY_ACTIVITY" && <ApplyActivity></ApplyActivity>}
+
+        {selectedMenu === "COMMUNITY" && <Community></Community>}
+      </div>
     </>
   );
 };
