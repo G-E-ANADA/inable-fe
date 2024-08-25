1f05a007ae49437dc79701b26bfcee18d77da07e
cogito1016 <cogito1016@naver.com>
2024-08-25 17:07:39 +0900

    [feat] 교육정보 > 취업정보 > 모달UI (kan-41)

--- src/pages/EducationPage.tsx
+++ src/pages/EducationPage.tsx
@@ -1,14 +1,41 @@
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent, useState, useEffect } from "react";
 import styles from "../css/Education.module.css";
 import Header from "../components/Header";
 import EducationSNB from "../components/EducationSNB";
 import jobInfoDummy_1 from "../asset/jobInfoDummy_1.svg";
 import jobInfoDummy_2 from "../asset/jobInfoDummy_2.svg";
+import leftBtn from "../asset/leftBtn.svg";
+import rightBtn from "../asset/rightBtn.svg";
+import cancelBtn from "../asset/cancelBtn.svg";
 
 type MyPageSNBMenu = "INFO" | "JOB_INFO" | "COMMUNITY";
 
 const EducationPage: FunctionComponent = () => {
   const [selectedMenu, setSelectedMenu] = useState("JOB_INFO");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  // 모달 바깥을 클릭했을 때 모달이 닫히도록 처리
+  useEffect(() => {
+    if (isModalOpen) {
+      const handleOutsideClick = (e: any) => {
+        if (e.target.className === styles.modalBackground) {
+          closeModal();
+        }
+      };
+      document.addEventListener("click", handleOutsideClick);
+      return () => {
+        document.removeEventListener("click", handleOutsideClick);
+      };
+    }
+  }, [isModalOpen]);
 
   const handleMenuSelect = (menu: MyPageSNBMenu) => {
     setSelectedMenu(menu);
@@ -29,7 +56,7 @@ const EducationPage: FunctionComponent = () => {
               <div className={styles.tip1}>{`취업 정보 & TIP`}</div>
             </div>
             <div className={styles.frameParent}>
-              <div className={styles.frameGroup}>
+              <div className={styles.frameGroup} onClick={openModal}>
                 <img
                   className={styles.frameChild}
                   alt=""
@@ -63,6 +90,44 @@ const EducationPage: FunctionComponent = () => {
           </div>
         )}
         {selectedMenu === "COMMUNITY" && <div>2</div>}
+
+        {isModalOpen && (
+          <div className={styles.modalBackground}>
+            <div className={styles.modalContainer}>
+              <div className={styles.modalContentContainer}>
+                <div className={styles.modalLabel}>
+                  AI 도우미를 활용하여 취업 성공하기
+                </div>
+                <div className={styles.content}>
+                  <div className={styles.cardContainer}>
+                    <img className={styles.modalPassBtn} alt="" src={leftBtn} />
+
+                    <div className={styles.frameGroup}>
+                      <img
+                        className={styles.frameChild}
+                        alt=""
+                        src={jobInfoDummy_1}
+                      />
+                    </div>
+
+                    <img
+                      className={styles.modalPassBtn}
+                      alt=""
+                      src={rightBtn}
+                    />
+                  </div>
+                  <div className={styles.modalLabel}>1/4</div>
+                </div>
+              </div>
+              <img
+                className={styles.cancelIcon}
+                onClick={closeModal}
+                alt=""
+                src={cancelBtn}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
