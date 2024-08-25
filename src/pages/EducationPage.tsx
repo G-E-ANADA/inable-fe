dac2b5d371f88e118641642dae87eb5f115f1a43
cogito1016 <cogito1016@naver.com>
2024-08-25 17:20:17 +0900

    [feat] 모달 카드 이전/다음 기능 구현 (kan-41)

--- src/pages/EducationPage.tsx
+++ src/pages/EducationPage.tsx
@@ -4,6 +4,9 @@ import Header from "../components/Header";
 import EducationSNB from "../components/EducationSNB";
 import jobInfoDummy_1 from "../asset/jobInfoDummy_1.svg";
 import jobInfoDummy_2 from "../asset/jobInfoDummy_2.svg";
+import jobInfoDummy_1_inner_2 from "../asset/jobInfoDummy_1_inner_2.svg";
+import jobInfoDummy_1_inner_3 from "../asset/jobInfoDummy_1_inner_3.svg";
+import jobInfoDummy_1_inner_4 from "../asset/jobInfoDummy_1_inner_4.svg";
 import leftBtn from "../asset/leftBtn.svg";
 import rightBtn from "../asset/rightBtn.svg";
 import cancelBtn from "../asset/cancelBtn.svg";
@@ -13,6 +16,28 @@ type MyPageSNBMenu = "INFO" | "JOB_INFO" | "COMMUNITY";
 const EducationPage: FunctionComponent = () => {
   const [selectedMenu, setSelectedMenu] = useState("JOB_INFO");
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const images = [
+    jobInfoDummy_1,
+    jobInfoDummy_1_inner_2,
+    jobInfoDummy_1_inner_3,
+    jobInfoDummy_1_inner_4,
+  ];
+
+  // 현재 보여지는 이미지의 인덱스를 추적하는 상태를 추가합니다.
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // 다음 이미지로 넘어가는 함수입니다.
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  // 이전 이미지로 돌아가는 함수입니다.
+  const handlePrevious = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
+  };
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -100,23 +125,31 @@ const EducationPage: FunctionComponent = () => {
                 </div>
                 <div className={styles.content}>
                   <div className={styles.cardContainer}>
-                    <img className={styles.modalPassBtn} alt="" src={leftBtn} />
+                    <img
+                      className={styles.modalPassBtn}
+                      onClick={handlePrevious}
+                      alt=""
+                      src={leftBtn}
+                    />
 
                     <div className={styles.frameGroup}>
                       <img
                         className={styles.frameChild}
                         alt=""
-                        src={jobInfoDummy_1}
+                        src={images[currentIndex]}
                       />
                     </div>
 
                     <img
                       className={styles.modalPassBtn}
+                      onClick={handleNext}
                       alt=""
                       src={rightBtn}
                     />
                   </div>
-                  <div className={styles.modalLabel}>1/4</div>
+                  <div className={styles.modalLabel}>
+                    {currentIndex + 1}/{images.length}
+                  </div>
                 </div>
               </div>
               <img
